b59302cf908185c275cc9a816512450db87d6b8f
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-09-24 20:45:12 -0400

    pass test

--- printtest.rb
+++ printtest.rb
@@ -34,6 +34,39 @@ def test_10
 	assert_equal("mined minds", fizz_buzz(90))
 end
 def test_11
-	assert_equal("minded", fizz_buzz(12))
+	assert_equal("mined", fizz_buzz(12))
+end
+def test_12
+	assert_equal("minds", fizz_buzz(25))
+end
+def test_13
+	assert_equal("mined minds", fizz_buzz(60))
+end
+def test_14
+	assert_equal("mined minds", fizz_buzz(30))
+end
+def test_15
+	assert_equal("mined minds", fizz_buzz(45))
+end
+def test_16
+	assert_equal("mined", fizz_buzz(18))
+end
+def test_17
+	assert_equal("mined", fizz_buzz(24))
+end
+def test_18
+	assert_equal("minds", fizz_buzz(35))
+end
+def test_19
+	assert_equal("minds", fizz_buzz(65))
+end
+def test_20
+	assert_equal("minds", fizz_buzz(70))
+end
+def test_21
+	assert_equal("minds", fizz_buzz(80))
+end
+def test_22
+	assert_equal("mined minds", fizz_buzz(75))
 end
 end
