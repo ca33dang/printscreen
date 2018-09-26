de67dbacdeef36b424c768bffc9c2a770014dec6
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-09-26 14:17:52 -0400

    pass all numbers

--- print.rb
+++ print.rb
@@ -1,11 +1,18 @@
 def fizz_buzz(number)
 	if (number % 15 == 0)
 		"mined minds"
-	elsif(number % 3 == 0)	
+	elsif(number % 5 == 0)	
 		"mined"
-	elsif (number % 5 == 0)
+	elsif (number % 3 == 0)
 		"minds"
 	else
 		number
 	end	
 end
+
+number = 1
+
+while number <= 100
+	p fizz_buzz(number)
+	number += 1
+	end
--- printtest.rb
+++ printtest.rb
@@ -7,37 +7,37 @@ def test_1
 	assert_equal("mined minds", fizz_buzz(15))
 end
 def test_2
-	assert_equal("minds", fizz_buzz(55))
+	assert_equal("mined", fizz_buzz(55))
 end
 def test_3
 	assert_equal("mined minds", fizz_buzz(30))
 end
 def test_4
-	assert_equal("minds", fizz_buzz(10))
+	assert_equal("mined", fizz_buzz(10))
 end
 def test_5
-	assert_equal("mined", fizz_buzz(3))
+	assert_equal("minds", fizz_buzz(3))
 end
 def test_6
-	assert_equal("minds", fizz_buzz(5))
+	assert_equal("mined", fizz_buzz(5))
 end
 def test_7
 	assert_equal("mined minds", fizz_buzz(45))
 end
 def test_8
-	assert_equal("mined", fizz_buzz(9))
+	assert_equal("minds", fizz_buzz(9))
 end
 def test_9
-	assert_equal("minds", fizz_buzz(20))
+	assert_equal("mined", fizz_buzz(20))
 end
 def test_10
 	assert_equal("mined minds", fizz_buzz(90))
 end
 def test_11
-	assert_equal("mined", fizz_buzz(12))
+	assert_equal("minds", fizz_buzz(12))
 end
 def test_12
-	assert_equal("minds", fizz_buzz(25))
+	assert_equal("mined", fizz_buzz(25))
 end
 def test_13
 	assert_equal("mined minds", fizz_buzz(60))
@@ -49,22 +49,22 @@ def test_15
 	assert_equal("mined minds", fizz_buzz(45))
 end
 def test_16
-	assert_equal("mined", fizz_buzz(18))
+	assert_equal("minds", fizz_buzz(18))
 end
 def test_17
-	assert_equal("mined", fizz_buzz(24))
+	assert_equal("minds", fizz_buzz(24))
 end
 def test_18
-	assert_equal("minds", fizz_buzz(35))
+	assert_equal("mined", fizz_buzz(35))
 end
 def test_19
-	assert_equal("minds", fizz_buzz(65))
+	assert_equal("mined", fizz_buzz(65))
 end
 def test_20
-	assert_equal("minds", fizz_buzz(70))
+	assert_equal("mined", fizz_buzz(70))
 end
 def test_21
-	assert_equal("minds", fizz_buzz(80))
+	assert_equal("mined", fizz_buzz(80))
 end
 def test_22
 	assert_equal("mined minds", fizz_buzz(75))
@@ -73,6 +73,9 @@ def test_23
 	assert_equal(71, fizz_buzz(71))
 end
 def test_24
-	assert_equal(72, fizz_buzz(72))
+	assert_equal("minds", fizz_buzz(72))
+end
+def test_25
+	assert_equal("minds", fizz_buzz(69))
 end
 end
